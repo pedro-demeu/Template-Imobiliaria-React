2a149ff1908cecd5aee58049870ad47d1a82be60
Pedro Demeu <demeu.pedro@gmail.com>
2021-02-19 11:18:55 -0300

    adicionando rotas ao site

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Routes, BrowserRouter, Route } from 'react-router-dom';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+import Home from './Components/Home';
+import Compra from './Components/Compra/Compra';
+import Aluguel from './Components/Aluguel/Aluguel';
+import Ajuda from './Components/Ajuda/Ajuda';
+import Login from './Components/Login/Login';
 
 function App() {
     return (
         <div className="App">
-            <h1>App react</h1>
+            <BrowserRouter>
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Home/>} />
+                    <Route path="/Compra" element={<Compra/>} />
+                    <Route path="/Aluguel" element={<Aluguel/>} />
+                    <Route path="/Ajuda" element={<Ajuda/>} />
+                    <Route path="/Login" element={<Login/>} />
+                </Routes>
+                <Footer />
+            </BrowserRouter>
         </div>
     )
 }
